0993d5f483698e9a1bbfac79252bc5405a2a8ba9
saurav_anand <anandsaurav100@gmail.com>
2020-04-30 16:29:39 +0530

    changes done in master branch after creating bugfix branch

--- SourceCode/Numbers.c
+++ SourceCode/Numbers.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main()
 {
+	//changes in master branch
+  int a = sub(10,8);
+	printf("%d",a);
   printf("12");
 }
 int sub(int a,int b)
 {
-	return a-b;
+	return (a-b);
 }
 //added commit in branch
 //bug resolving code added (a/b) i.e b!=0
